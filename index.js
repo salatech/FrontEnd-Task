46b9aa16398ffc13a6665a3dcba025bbc37d2f92
salatech <solahudeenbabatunde@gmail.com>
2024-07-06 17:59:22 +0100

    fix: fixed the errors

--- index.js
+++ index.js
@@ -2,75 +2,75 @@ const trackingData = [
 	{ type: "date", iconContent: "30", date: "WED", month: "July", content: "" },
 	{
 		type: "task",
-		iconContent: "img/Ellipse 400.svg",
+		iconContent: "assets/Ellipse 400.svg",
 		date: "11:30 AM",
 		content: "New task assigned to you",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 401.svg",
+		iconContent: "assets/Ellipse 401.svg",
 		date: "11:30 AM",
 		content: "New feedback received for task name",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 402.svg",
+		iconContent: "assets/Ellipse 402.svg",
 		date: "11:30 AM",
 		content: "Task marked completed by you",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 403.svg",
+		iconContent: "assets/Ellipse 403.svg",
 		date: "11:30 AM",
 		content: "Task Name updated for journey Name",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 404.svg",
+		iconContent: "assets/Ellipse 404.svg",
 		date: "11:30 AM",
 		content: "Follow up on feedback for Task Name completed on date XX/YY/ZZZZ",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{ type: "date", iconContent: "29", date: "THU", month: "July", content: "" },
 	{
 		type: "task",
-		iconContent: "img/Ellipse 405.svg",
+		iconContent: "assets/Ellipse 405.svg",
 		date: "11:30 AM",
 		content: "Meeting with Bobbie Charlie",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 406.svg",
+		iconContent: "assets/Ellipse 406.svg",
 		date: "11:30 AM",
 		content: "Chat about design mentorship",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 407.svg",
+		iconContent: "assets/Ellipse 407.svg",
 		date: "11:30 AM",
 		content: "Critical delay observed in time line adherence",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 	{
 		type: "task",
-		iconContent: "img/Ellipse 408.svg",
+		iconContent: "assets/Ellipse 408.svg",
 		date: "11:30 AM",
 		content: "Finish project documentation for review (1-1 checking)",
-		img: "img/Photo.png",
+		assets: "assets/Photo.png",
 		name: "Sheila O'Reily",
 	},
 ];
@@ -96,7 +96,7 @@ const createTrackingItem = (item) => {
                 <img src="${item.iconContent}" alt="">
             </div>
             <div class="tracking-date">${item.date}</div>
-            <div class="tracking-content">${item.content}<span style="display: flex;"><span class="content-image"><img src="${item.img}" alt=""></span>${item.name}</span></div>
+            <div class="tracking-content">${item.content}<span style="display: flex;"><span class="content-image"><img src="${item.assets}" alt=""></span>${item.name}</span></div>
         `;
 	}
 
